d9aeb50ccfb98f979a4eb9a20d1af9325fd95ea5
MaxTheTech <maxpetermail@gmail.com>
2024-04-15 14:14:37 +0200

    Added some to Activity + Non-project and project activities

--- src/main/java/dtu/projectManagementSystem/domain/Activity.java
+++ src/main/java/dtu/projectManagementSystem/domain/Activity.java
@@ -1,4 +1,27 @@
 package dtu.projectManagementSystem.domain;
 
+import dtu.projectManagementSystem.app.DateServer;
+
+import java.util.Calendar;
+
 public abstract class Activity {
+    private String name;
+    private int id;
+
+    public Activity(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public abstract String getTypeName() ;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+
 }
--- src/main/java/dtu/projectManagementSystem/domain/NonProjectActivity.java
+++ src/main/java/dtu/projectManagementSystem/domain/NonProjectActivity.java
@@ -1,4 +1,33 @@
 package dtu.projectManagementSystem.domain;
 
-public class NonProjectActivity {
+import dtu.projectManagementSystem.app.DateServer;
+
+public class NonProjectActivity extends Activity {
+
+    private DateServer startingDay;
+    private int durationDays;
+    public NonProjectActivity(String name, int id) {
+        super(name, id);
+    }
+
+    public void setStartingDay(DateServer startingDay) {
+        this.startingDay = startingDay;
+    }
+
+    public DateServer getStartingDay() {
+        return this.startingDay;
+    }
+
+    public void setDurationDays(int durationDays) {
+        this.durationDays = durationDays;
+    }
+
+    public int getDurationDays() {
+        return this.durationDays;
+    }
+
+    public String getTypeName() {
+        return "Non-Project Activity";
+    }
+
 }
