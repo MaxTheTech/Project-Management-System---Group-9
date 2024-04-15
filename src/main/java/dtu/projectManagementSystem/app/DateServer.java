07b07a67430e3bf00b88400e304b763cafbc48a2
TastyTanya <ladefod@gmail.com>
2024-04-15 13:53:14 +0200

    DateServer oprettet

--- src/main/java/dtu/projectManagementSystem/app/DateServer.java
+++ src/main/java/dtu/projectManagementSystem/app/DateServer.java
@@ -1,4 +1,33 @@
 package dtu.projectManagementSystem.app;
 
 public class DateServer {
+
+    enum day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+    private int weekDay = day.Monday.ordinal();
+    private int week = 1;
+    public final int year = 2024;
+
+
+    public DateServer(){};
+    public int getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(int weekDay) {
+        this.weekDay = weekDay;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
 }
