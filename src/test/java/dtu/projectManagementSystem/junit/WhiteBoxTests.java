380f5e19bc470432066c7af502da1238a33c11d9
TastyTanya <ladefod@gmail.com>
2024-05-02 11:33:55 +0200

    Første test virker

--- src/test/java/dtu/projectManagementSystem/junit/WhiteBoxTests.java
+++ src/test/java/dtu/projectManagementSystem/junit/WhiteBoxTests.java
@@ -1,12 +1,31 @@
 package dtu.projectManagementSystem.junit;
 
 import dtu.projectManagementSystem.app.SoftwareHuset;
+import dtu.projectManagementSystem.domain.Project;
+import dtu.projectManagementSystem.info.EmployeeInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class WhiteBoxTests {
 
-    private SoftwareHuset softwareHuset;
+    private final SoftwareHuset softwareHuset = new SoftwareHuset();
 
-    public void createProject_TestSetA(){
+    @Test
+    public void createProject_TestSetA() throws Exception {
+        //Setup
+        EmployeeInfo admin = new EmployeeInfo("SebA");
+        softwareHuset.registerEmployee(admin);
+        softwareHuset.employeeLogin("SebA");
+
+        //Make sure that projectRepository is empty as specified in the white-box planning section
+        Assertions.assertEquals(0, softwareHuset.getProjectRepository().size());
+
+        //Create project and test that it was successfully added
+        softwareHuset.createProject("testProject");
+        Assertions.assertTrue(softwareHuset.getProjectRepository().stream().
+                anyMatch(i -> i.getProjectName().equals("testProject")));
 
     }
 
